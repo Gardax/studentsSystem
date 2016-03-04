cf2fa964976f2787d5cce57ce4e2d66b5653c29e
Georgi Georgiev <gardax95@gmail.com>
2016-03-04 20:14:02 +0200

    UI moved to different bundle.

---
+++ studentsSystem/src/UIBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace UIBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('UIBundle:Default:index.html.twig', []);
+    }
+}
