a621a96942eb4ba641d33e635fceb96c329dca56
suxorr <hristevmario@gmail.com>
2016-03-17 18:34:09 +0200

    fix paging buttons on student assessment page and display the results from - to and maxresults

--- studentsSystem/src/ExportBundle/Controller/ExportController.php
+++ studentsSystem/src/ExportBundle/Controller/ExportController.php
@@ -30,7 +30,6 @@ class ExportController extends Controller
 
         $response->setCallback(
             function () use ($users) {
-
                 $handle = fopen('php://output', 'r+');
 
                 $data = array(
@@ -57,4 +56,214 @@ class ExportController extends Controller
 
         return $response;
     }
+
+    /**
+     * @Route("/csv/students")
+     */
+    public function studentCSVAction()
+    {
+        $service = $this->get('student_service');
+
+        $students = $service->getStudents(0, 0, [], false, false, true);
+
+        $response = new StreamedResponse();
+
+        $response->setCallback(
+            function () use ($students) {
+                $handle = fopen('php://output', 'r+');
+
+                $data = array(
+                    'Id',
+                    'First name',
+                    'Last name',
+                    'Email',
+                    'Faculty number'
+                );
+                fputcsv($handle, $data, ';');
+
+                foreach ($students as $student) {
+                    $data = array(
+                        $student->getId(),
+                        $student->getFirstName(),
+                        $student->getLastName(),
+                        $student->getEmail(),
+                        $student->getFacultyNumber()
+                    );
+                    fputcsv($handle, $data, ';');
+                }
+                fclose($handle);
+            }
+        );
+
+        $response->headers->set('Content-Type', 'application/force-download');
+        $response->headers->set('Content-Disposition', 'attachment; filename="students.csv"');
+
+        return $response;
+    }
+
+    /**
+     * @Route("/csv/courses")
+     */
+    public function courseCSVAction()
+    {
+        $service = $this->get('course_service');
+
+        $courses = $service->getCourses('all', 0);
+
+        $response = new StreamedResponse();
+
+        $response->setCallback(
+            function () use ($courses) {
+                $handle = fopen('php://output', 'r+');
+
+                $data = array(
+                    'Id',
+                    'Name'
+                );
+                fputcsv($handle, $data, ';');
+
+                foreach ($courses as $course) {
+                    $data = array(
+                        $course->getId(),
+                        $course->getName()
+                    );
+                    fputcsv($handle, $data, ';');
+                }
+                fclose($handle);
+            }
+        );
+
+        $response->headers->set('Content-Type', 'application/force-download');
+        $response->headers->set('Content-Disposition', 'attachment; filename="courses.csv"');
+
+        return $response;
+    }
+
+    /**
+     * @Route("/csv/subjects")
+     */
+    public function subjectCSVAction()
+    {
+        $service = $this->get('subject_service');
+
+        $subjects = $service->getAllSubjects();
+
+        $response = new StreamedResponse();
+
+        $response->setCallback(
+            function () use ($subjects) {
+                $handle = fopen('php://output', 'r+');
+
+                $data = array(
+                    'Id',
+                    'Name',
+                    'Workload Lectures',
+                    'Workload Exercises'
+                );
+                fputcsv($handle, $data, ';');
+
+                foreach ($subjects as $subject) {
+                    $data = array(
+                        $subject->getId(),
+                        $subject->getName(),
+                        $subject->getWorkloadLectures(),
+                        $subject->getWorkloadExercises()
+                    );
+                    fputcsv($handle, $data, ';');
+                }
+                fclose($handle);
+            }
+        );
+
+        $response->headers->set('Content-Type', 'application/force-download');
+        $response->headers->set('Content-Disposition', 'attachment; filename="subjects.csv"');
+
+        return $response;
+    }
+
+    /**
+     * @Route("/csv/specialities")
+     */
+    public function specialityCSVAction()
+    {
+        $service = $this->get('speciality_service');
+
+        $specialities = $service->getSpecialities('all', 0, []);
+
+        $response = new StreamedResponse();
+
+        $response->setCallback(
+            function () use ($specialities) {
+                $handle = fopen('php://output', 'r+');
+
+                $data = array(
+                    'Id',
+                    'Long name',
+                    'Short name'
+                );
+                fputcsv($handle, $data, ';');
+
+                foreach ($specialities as $speciality) {
+                    $data = array(
+                        $speciality->getId(),
+                        $speciality->getSpecialityLongName(),
+                        $speciality->getSpecialityShortName()
+                    );
+                    fputcsv($handle, $data, ';');
+                }
+                fclose($handle);
+            }
+        );
+
+        $response->headers->set('Content-Type', 'application/force-download');
+        $response->headers->set('Content-Disposition', 'attachment; filename="specialities.csv"');
+
+        return $response;
+    }
+
+    /**
+     * @Route("/csv/assessments")
+     */
+    public function assessmentCSVAction()
+    {
+        $assessmentService = $this->get('student_assessment_service');
+
+        $assessments = $assessmentService->getStudentAssessments(0, 0, [], false, true);
+
+        $response = new StreamedResponse();
+
+        $response->setCallback(
+            function () use ($assessments) {
+                $handle = fopen('php://output', 'r+');
+
+                $data = array(
+                    'Id',
+                    'First name',
+                    'Last name',
+                    'Subject',
+                    'Assessment'
+                );
+                fputcsv($handle, $data, ';');
+                foreach ($assessments as $assessment) {
+                    $data = array(
+                        $assessment->getId(),
+                        $assessment->getStudent()->getFirstName(),
+                        $assessment->getStudent()->getLastName(),
+                        $assessment->getSubject()->getName(),
+                        $assessment->getAssessment()
+                    );
+                    fputcsv($handle, $data, ';');
+                }
+
+                fclose($handle);
+            }
+
+        );
+
+        $response->headers->set('Content-Type', 'application/force-download');
+        $response->headers->set('Content-Disposition', 'attachment; filename="assessments.csv"');
+
+        return $response;
+    }
+
 }
