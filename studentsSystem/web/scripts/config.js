fb292db4bc016323aad7e158a557df9d38e7caf0
suxorr <hristevmario@gmail.com>
2016-03-19 00:42:55 +0200

    fix the position of the profile button and the logou button

--- studentsSystem/web/scripts/config.js
+++ studentsSystem/web/scripts/config.js
@@ -2,11 +2,11 @@ var config = (function (){
 
     return {
         //API_URL : "http://127.0.0.1:8000/api/"
-        //API_URL : "http://192.168.0.101:8000/api/" //-Mario desktop
+        API_URL : "http://192.168.0.104:8000/api/" //-Mario desktop
         //API_URL : "http://students-system:8000/api/" //-Mario laptop
 
         //API_URL : "http://192.168.0.102:8000/api/"
-        API_URL : "http://192.168.0.106:3000/api/"
+        //API_URL : "http://192.168.0.106:3000/api/"
 
     };
 })();
